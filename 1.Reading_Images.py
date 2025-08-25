cdba733c99d8282d81115e0023c4d26f99084963
swaroop <swaroopyadav49@gmail.com>
2025-08-25 13:56:50 +0530

    file_reading_added

--- 1.Reading_Images.py
+++ 1.Reading_Images.py
@@ -3,11 +3,12 @@ import cv2
 ## Reading images
 
 img = cv2.imread("resources/lena.png")
-
 print(img.shape)
-cv2.imshow("Output", img)   # showing the image in console, with out waitkey it will close immediately.
-cv2.waitKey(0)
+cv2.imshow("Output", img)   # showing the image in console, without waitkey it will close immediately.
+cv2.waitKey(0)  # waitkey is used to hold the image for infinite or longer time until any key is pressed.
+
+
 
 ## Reading videos
 
-cv2.videoCapture(0)  # 0 means webcam, 1 means external camera
+#cap = cv2.videoCapture("resources/elon.mp4")  # 0 means webcam, 1 means external camera
